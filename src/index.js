e3de56477076084586b3c133a6c93becf6e0210c
Akhtar <reachtoakhtar@gmail.com>
2020-03-29 20:21:07 +0530

    Add redux-devtools-extension to debug redux app.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import {bugAdded, bugRemoved, bugResolved} from './actions';
 import store from './store';
+import {bugAdded, bugRemoved, bugResolved} from './actions';
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState())
 })
 
 store.dispatch(bugAdded("Bug 1"))
+store.dispatch(bugAdded("Bug 2"))
+store.dispatch(bugAdded("Bug 3"))
 
 store.dispatch(bugResolved(1))
 
