4c6e6f6399bd83666a814c6b14c01d3be2e77239
Akhtar <reachtoakhtar@gmail.com>
2020-04-06 18:04:55 +0530

    Ducks pattern for managing store.

---
+++ src/store/configureStore.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Akhtar on 22/03/2020.
+ */
+
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import reducer from './bugs';
+
+
+export default function configureStore() {
+  const store = createStore(reducer, devToolsEnhancer({ trace: true }))
+  return store
+}
